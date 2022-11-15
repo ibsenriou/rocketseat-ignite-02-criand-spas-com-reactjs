e879583bd43069cebb9cce7ebdcae53ef1218bb6
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-11-15 18:15:27 -0300

    Formulários.

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export const History = () => {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Estudar React</td>
+              <td>2 horas</td>
+              <td>Há 10 minutos</td>
+              <td>
+                <Status statusColor={'green'}>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>2 horas</td>
+              <td>Há 10 minutos</td>
+              <td>
+                <Status statusColor={'red'}>Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>2 horas</td>
+              <td>Há 10 minutos</td>
+              <td>
+                <Status statusColor={'green'}>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>2 horas</td>
+              <td>Há 10 minutos</td>
+              <td>
+                <Status statusColor={'yellow'}>Em andamento</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,83 @@
 import { Play } from 'phosphor-react'
 
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
   StartCountdownButton,
+  TaskInput,
 } from './styles'
 
+const newCycleFormSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no minimo 60 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
+// ** Interface de Tipagem de FormData de Formulário SEM Inferecia de Tipagem
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+
+// ** Interface de Tipagem de FormData de Formulário COM Inferecia de Tipagem
+type NewCycleFormData = zod.infer<typeof newCycleFormSchema>
+
 export const Home = () => {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            list="task-sugestions"
+            placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
+          />
+
+          <datalist id="task-sugestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 1" />
+            <option value="Projeto 1" />
+            <option value="Playstation" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -30,7 +90,7 @@ export const Home = () => {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
